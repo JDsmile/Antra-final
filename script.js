e552c893fc6ca9a11a9e1d8a558db84a535e5819
Yusen Jiang <yusenjia@buffalo.edu>
2022-08-01 16:03:01 -0700

    page reload when delete a todo

--- script.js
+++ script.js
@@ -13,11 +13,14 @@ function display(data){
         <p>${item.title}
         </p>
         <div>
+
+
           <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small" class="small edit" fill="#e6e2d3"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg>
+
           <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small" class="small delete" onclick="deleteTodo(${item.id})" fill="#e6e2d3"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
           </div>
-     </div>
-      `
+        
+     </div>    `
   })
   document.querySelector('.todos-container').innerHTML = todos.join('')
 
@@ -26,6 +29,7 @@ function display(data){
 //bug
 function deleteTodo(id) {
   fetch('http://localhost:3000/todos/'+id, { method: 'DELETE' })
+  window.location.reload();
 }
 const inputBox = document.querySelector(".form-input")
 const formSubmit = document.querySelector(".form-submit")

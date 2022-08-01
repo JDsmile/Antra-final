4816d48e381a898043b8ab1c922e6144f974cc78
Yusen Jiang <yusenjia@buffalo.edu>
2022-08-01 11:46:25 -0700

    post data to server

--- script.js
+++ script.js
@@ -7,12 +7,21 @@ const inputBox = document.querySelector(".form-input")
 const formSubmit = document.querySelector(".form-submit")
 let input =""
 
+let data;
+
 inputBox.addEventListener("input", (event)=>{
   input = event.target.value
-
+  data = {"title":input,"completed":false}
 })
 
 formSubmit.addEventListener("click", (event)=>{
 event.preventDefault()
-console.log(input)
+fetch('http://localhost:3000/todos',{
+    method :"POST",
+    headers : {
+      'Content-Type': "application/json"
+    },
+    body: JSON.stringify(data)
+  })
 })
+

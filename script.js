c91c6fa025f7ad03268e571b01bb8e3ac1f5de96
Yusen Jiang <yusenjia@buffalo.edu>
2022-08-02 01:36:58 -0700

    move completed todo to the bottom (bug)

--- script.js
+++ script.js
@@ -7,26 +7,51 @@ const formSubmit = document.querySelector(".form-submit")
 let input =""
 let data;
 let canEdit = false
-let todoList = []
 
-function loadData(){
-  fetch(`${API_URL}${PATH}`)
-  .then((response) => response.json())
-  .then((data)=>display(data))
+
+async function loadData(){
+  const response = await fetch(`${API_URL}${PATH}`)
+  let todoList = await response.json()
+
+  todoList.map((item,index)=>{
+    if(item.completed===true){ 
+      todoList.splice(index,1)
+      todoList.push(item)
+    }
+    return todoList
+  })
+
+  // for(let i =0;i<todoList.length;i++){
+  //   // if(todoList[i].completed===true){
+  //   //   let completedTodo = todoList[i]
+  //   //   todoList.splice(i,1)
+  //   //   todoList.push(completedTodo)
+  //   // }
+
+  //   if(todoList[i].completed===true){
+  //     todoList.push(...todoList.splice(i,1))
+  //     todoList.splice(todoList.length,0,todoList.splice(i,1)[0])
+  //   }
+
+  // }
+
+  console.log(todoList)
+
+
+  // .then((data)=>display(data))
+  display(todoList)
 
 }
 
-console.log(todoList)
+
 
 function display(data){
   const todos = data.map((item,index) =>{
     
     return `  
       <div class="todo-item">
-
         <p class="todo-content ${item.completed ? "completed": "" }" id=${item.id} onclick="completedTodo(${item.id},${item.completed})">${item.title}
         </p>
-
         <div>
           <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small" class="small edit" onclick="editTodo(${item.id})" fill="#e6e2d3"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg>
 
